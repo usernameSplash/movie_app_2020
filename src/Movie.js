4faf8e43c90fc4af3fe8ea18de924b72fb07d438
usernameSplash <kkfenix3085@naver.com>
2020-02-20 00:22:51 +0900

    Add HTML, and showing more data(year, summary, poster)

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, year, title, summary, poster }) {
-  console.log(title);
-  return <h5>{title}</h5>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.number,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
